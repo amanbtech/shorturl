5a292e056eaf70c3ff6dc4451790c3465bdd2ca6
amanbtech <amanbtech2526@gmail.com>
2026-08-07 17:34:25 +0530

    bug in delte

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -31,6 +31,12 @@ def test_redirect():
             "expiry_days": 8
         }
     )
+    print(login_response.status_code)
+    print(login_response.json())
+
+    print(data_response.status_code)
+    print(data_response.text)
+    print(data_response.json())
     short_code=data_response.json()["short_code"]
     delete_response=client.delete(f"/delete/{short_code}",headers={"Authorization":f"Bearer{token}"})
     assert delete_response.status_code == 200
