811dd6e9e07f82ce916dd96d7b227b1dcc03b652
amanbtech <amanbtech2526@gmail.com>
2026-08-07 17:26:44 +0530

    fix bug in login

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,20 @@ import uuid
 
 from fastapi.testclient import TestClient
 from main import  app
+
+
 login_client=TestClient(app)
-response=login_client.post("/login", json={"username":f"text_{uuid.uuid4().hex}", "password_hash": "aman@123"})
-assert response.status_code==200
+name=f"text_{uuid.uuid4().hex}"
+gmail=f"text_{uuid.uuid4().hex}"
+def login():
+    login_client.post("/signup",json={
+        "username":name,
+        "email":gmail,
+        "password_hash":"aman@123"
+
+    })
+    response=login_client.post("/login", json={"username":name, "password_hash": "aman@123"})
+    print(response.status_code)
+    print(response.text)
+    print(response.json())
+    assert response.status_code==200
